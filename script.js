41020cb3f59b67f101dec434ae2ae6ec706df678
Badal kumar singh <jpbadalsingh8899@gmail.com>
2021-05-10 14:55:52 +0530

    BigBann

---
+++ script.js
@@ -0,0 +1,14 @@
+var typed = new Typed('.type', {
+  strings: ['You Think ,', 'We   Innovate'],
+  typeSpeed: 60,
+  backSpeed: 60,
+  loop: true
+});
+
+$(document).ready(function(){
+    $(".dropdown-trigger").dropdown();
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+    $('.slider').slider();
+  
+  });
